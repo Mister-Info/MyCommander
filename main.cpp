bbefebc8ba96fa2018e9a24c8aa02ecc48298034
Mister-Info <tudor.ezaru@yahoo.com>
2022-11-28 21:31:02 +0200

    add: file navigation

--- main.cpp
+++ main.cpp
@@ -3,16 +3,54 @@
 #include <stdlib.h>
 #include<cstring>
 #include<windows.h>
+///windows.h este folosit pentru a lista fisierele
 #include <graphics.h>
 #include <winbgim.h>
 
 
 using namespace std;
 
-void indexare_foldere_fisiere(char p[], WIN32_FIND_DATA cautare, char foldere[101][257],
-                              char fisiere[101][257], int &nr_foldere, int &nr_fisiere)
-                              ///functia asta gaseste folderele si fisierele
+
+void clear_stanga()
 {
+    /// Background alb
+    setcolor(DARKGRAY);
+    setfillstyle(SOLID_FILL, WHITE);
+    bar3d(10, 80,  497, 688, 0, 0);
+
+    /// Nume, extensie, etc
+    setcolor(COLOR(157, 157, 195));
+    setfillstyle(SOLID_FILL, COLOR(233, 236, 255));
+    bar3d(10, 80, 497, 105, 0, 0);
+    setcolor(BLACK);
+    setbkcolor(COLOR(233, 236, 255));
+    outtextxy(15, 85, "Name");
+}
+
+void clear_dreapta()
+{
+    /// Background alb
+    setcolor(DARKGRAY);
+    setfillstyle(SOLID_FILL, WHITE);
+    bar3d(537, 80, 1014, 688, 0, 0);
+
+    /// Nume, extensie, etc
+    setcolor(COLOR(157, 157, 195));
+    setfillstyle(SOLID_FILL, COLOR(233, 236, 255));
+    bar3d(537, 80, 1014, 105, 0, 0);
+    setcolor(BLACK);
+    setbkcolor(COLOR(233, 236, 255));
+    outtextxy(542, 85, "Name");
+}
+
+void indexare_foldere_fisiere(char cale[],char **foldere, char **fisiere, int &nr_foldere, int &nr_fisiere)
+///functia asta gaseste folderele si fisierele
+{
+
+    WIN32_FIND_DATA cautare;
+    char p[257];
+    strcpy(p,cale);
+    strcat(p,"\\*");
     nr_fisiere=nr_foldere=0;
 
     HANDLE director=FindFirstFile(p,&cautare);
@@ -31,67 +69,105 @@ void indexare_foldere_fisiere(char p[], WIN32_FIND_DATA cautare, char foldere[10
 
     }
 }
+/// char **p este un vector de vectori
+void afisare( char **foldere,char **fisiere, int nr_foldere,int nr_fisiere,int pozitiaCurenta,int x)
+{
+    int y=110;
 
-void afisareFoldere(int nrdisc, char discuri_locale[101][257], int pozitiaCurenta, bool stangaDreapta){
-     int x;
-     if (stangaDreapta==false) x=15;
-     else
-        x=542;
-     int y=110;
-     for(int i=0; i<nrdisc; i++)
+
+    for(int i=0; i<nr_foldere; i++)
+    {
+        if(i==pozitiaCurenta)///select
         {
-            if(i==pozitiaCurenta)
-            {
-                setcolor(BLACK);
-                setbkcolor(COLOR(149, 159, 255));
-                outtextxy(x,y,discuri_locale[i]);
-                y+=20;
-            }
-            else
-            {
-                setcolor(BLACK);
-                setbkcolor(WHITE);
-                outtextxy(x,y,discuri_locale[i]);
-                y+=20;
-            }
+            setcolor(BLACK);
+            setbkcolor(COLOR(149, 159, 255));
+            outtextxy(x,y,foldere[i]);
+            y+=20;
+
         }
+        else
+        {
+            setcolor(BLACK);
+            setbkcolor(WHITE);
+            outtextxy(x,y,foldere[i]);
+            y+=20;
+        }
+    }
+    for(int i=0; i<nr_fisiere; i++)
+    {
+        if(i+nr_foldere==pozitiaCurenta)///fisierele sunt afisate dupa foldere
+        {
+            setcolor(BLACK);
+            setbkcolor(COLOR(149, 159, 255));
+            outtextxy(x,y,fisiere[i]);
+            y+=20;
+
+        }
+        else
+        {
+            setcolor(BLACK);
+            setbkcolor(WHITE);
+            outtextxy(x,y,fisiere[i]);
+            y+=20;
+        }
+    }
 }
 
-
-void cautare_start(char foldere[101][257], char fisiere[101][257],char discuri_locale[101][257],
-                          int &nr_foldere, int &nr_fisiere, WIN32_FIND_DATA cautare, char p[255], int &nrdisc, bool stangaDreapta)
+///indexarea pentru discuri
+void cautare_start(char **foldere,int &nr_foldere, int &nr_fisiere)
 {
+
+    WIN32_FIND_DATA cautare;
+    char p[255];
+
+    nr_fisiere=0;
+    nr_foldere=0;
     for(char c='A'; c<='Z'; c++)
     {
-        char disk[1];
+        char disk[2];
         disk[0]=c;
         disk[1]=NULL;
         strcpy(p,disk);
         strcat(p,":");///pentru viitoarele local discuri
         strcat(p,"\\*");
-        nr_foldere=nr_fisiere=0;
 
         HANDLE director=FindFirstFile(p,&cautare);///directorul explicit
 
         if(director!=INVALID_HANDLE_VALUE)///practic daca directorul ala nu are nimic
         {
-            char pcopie[1];
+            char pcopie[3];
             pcopie[0]=p[0];
-            pcopie[1]=NULL;
-            strcat(discuri_locale[nrdisc]," Local disk: ");///11
-            strcat(discuri_locale[nrdisc++],pcopie);
-            strcat(discuri_locale[nrdisc-1], " ");
+            pcopie[1]=':';
+            pcopie[2]=NULL;
+
+            strcpy(foldere[nr_foldere++],pcopie);
+
 
         }
-        afisareFoldere(nrdisc, discuri_locale, 0, stangaDreapta);
-        for (int i=0; i<nrdisc; i++)
-            indexare_foldere_fisiere(p, cautare,  foldere, fisiere, nr_foldere, nr_fisiere);
 
         FindClose(director);
 
     }
 }
 
+///listeaza fisierele si folderele dintr-o cale
+///cale goala = lista din discurile locale
+void reindex(char cale[], char **foldere, char **fisiere, int &nr_fisiere, int &nr_foldere)
+{
+
+    nr_foldere=nr_fisiere=0;
+    if(cale[0]==NULL)
+    {
+        cautare_start(foldere,nr_foldere,nr_fisiere);
+    }
+    else
+    {
+        indexare_foldere_fisiere(cale, foldere, fisiere,nr_foldere,nr_fisiere);
+    }
+}
+
+
+///Heads Up Display
 void HUD()
 {
     ///Partea de design a aplicatiei (interfata)
@@ -154,53 +230,169 @@ void HUD()
     rectangle(537, 80, 1013, 687);
 }
 
+/// Aloca memorie pentru o matrice NxM
+/// (vector de siruri de caractere)
+char** init_lista(int n, int m)
+{
+    /// Alocam N linii, adica spatiu pentru N pointeri
+    char** lista = (char**) malloc(n * sizeof(char*));///char* are 4 sau 8 bytes
+
+    for (int i = 0; i < n; ++i)
+    {
+        /// Alocam M coloane pentru linia i, adica spatiu pentru M caractere
+        lista[i] = (char*) malloc(m * sizeof(char));///char are 1 byte
+    }
+
+    return lista;
+}
+
 void utilizareaAplicatiei()
-{   char foldereStanga[101][257], fisiereStanga[101][257], discuri_localeStanga[101][257];///le voi optimiza mai tarziu
+{
+    ///marimile sunt hardcodate, dar pe viitor voi creste dinamic in functie de numarul de foldere
+    char** foldereStanga = init_lista(101, 257);
+    char** fisiereStanga = init_lista(101, 257);
+
+    char** foldereDreapta = init_lista(101, 257);
+    char** fisiereDreapta = init_lista(101, 257);
+
     int nr_foldereStanga, nr_fisiereStanga;
-    char foldereDreapta[101][257], fisiereDreapta[101][257], discuri_localeDreapta[101][257];///le voi optimiza mai tarziu
     int nr_foldereDreapta, nr_fisiereDreapta;
-    WIN32_FIND_DATA cautare;///pentru cautare
-    char pStanga[255], pDreapta[255];
-    int nrdiscStanga=0, nrdiscDreapta=0;
-    cautare_start(foldereStanga, fisiereStanga,discuri_localeStanga, nr_foldereStanga,
-                  nr_fisiereStanga, cautare, pStanga, nrdiscStanga, false);
-    cautare_start(foldereDreapta, fisiereDreapta,discuri_localeDreapta, nr_foldereDreapta,
-                  nr_fisiereDreapta, cautare, pDreapta, nrdiscDreapta, true);
-    bool loop=true, mousePress=false, tastaEsc=false, tastaTab=false, tastaDown=false, tastaUp=false,
-         careFereastra=false; ///false e stanga si true e dreapta
-    int depthStanga=0, depthDreapta=0; ///variabile de contorizare a adancimii fisierelor
-    int viewCounterStanga=0, viewCounterDreapta=0; ///variabile de contorizare a adancimii fisierelor
+
+    ///tastele apasate
+    bool loop=true, mousePress=false, tastaEsc=false, tastaTab=false, tastaDown=false, tastaUp=false,tastaEnter=false,careFereastra=false; ///false e stanga si true e dreapta
+
+    int viewCounterStanga=0, viewCounterDreapta=0; ///folderul/fisierul selectat
     int clickX, clickY;///coordonate mouse
+    char caleStanga[257], caleDreapta[257];
+
+    ///la inceput vom afisa discurile locale(adica, avem cale goala)
+    caleStanga[0]=NULL;
+    caleDreapta[0]=NULL;
+
+    ///listam discurile locale
+    reindex(caleStanga,foldereStanga,fisiereStanga,nr_fisiereStanga,nr_foldereStanga);
+    reindex(caleDreapta,foldereDreapta,fisiereDreapta,nr_fisiereDreapta,nr_foldereDreapta);
+
+    ///afisam discurile
+    afisare(foldereStanga,fisiereStanga,nr_foldereStanga,nr_fisiereStanga,viewCounterStanga,15);
+    afisare(foldereDreapta,fisiereDreapta,nr_foldereDreapta,nr_fisiereDreapta,viewCounterDreapta,542);
+
     while (loop)
     {
-        if (GetAsyncKeyState(VK_DOWN)){
-           if (!tastaDown){ tastaDown=true;
-              if (careFereastra==false && viewCounterStanga+1<nrdiscStanga){
-                 viewCounterStanga++;
-                 afisareFoldere(nrdiscStanga, discuri_localeStanga, viewCounterStanga, false);
-              }
-              else if(careFereastra==true && viewCounterDreapta+1<nrdiscDreapta) {
-                  viewCounterDreapta++;
-                  afisareFoldere(nrdiscDreapta, discuri_localeDreapta, viewCounterDreapta, true);
-              }
-           }
-           tastaDown=false;
-           Sleep(300);
+        ///Enter
+        if(GetAsyncKeyState(VK_RETURN))
+        {
+
+            if (!tastaEnter)
+            {
+                tastaEnter=true;
+
+                if (careFereastra==false)///stanga
+                {
+                    ///am selectat un fisier
+                    if(viewCounterStanga>=nr_foldereStanga)
+                    {
+                        ///enter pe un fisier
+                        ///TODO: de implementat
+                    }
+                    else
+                    {
+                        ///enter pe un folder
+                        if (caleStanga[0] != NULL)
+                        {
+                            strcat(caleStanga, "\\");
+                        }
+                        strcat(caleStanga,foldereStanga[viewCounterStanga]);
+
+                        viewCounterStanga = 0;
+
+                        reindex(caleStanga,foldereStanga,fisiereStanga,nr_fisiereStanga,nr_foldereStanga);
+                        clear_stanga();
+                        afisare(foldereStanga,fisiereStanga,nr_foldereStanga,nr_fisiereStanga,viewCounterStanga,15);
+                    }
+                }
+                else if(careFereastra==true)
+                {
+                    if(viewCounterDreapta>=nr_foldereDreapta)
+                    {
+                        ///enter pe un fisier
+                        ///TODO: de implementat
+                    }
+                    else
+                    {
+                        ///enter pe un folder
+                        if (caleDreapta[0] != NULL)
+                        {
+                            strcat(caleDreapta, "\\");
+                        }
+                        strcat(caleDreapta,foldereDreapta[viewCounterDreapta]);
+
+                        viewCounterDreapta = 0;
+
+                        reindex(caleDreapta,foldereDreapta,fisiereDreapta,nr_fisiereDreapta,nr_foldereDreapta);
+                        clear_dreapta();
+                        afisare(foldereDreapta,fisiereDreapta,nr_foldereDreapta,nr_fisiereDreapta,viewCounterDreapta,542);
+                    }
+                }
+            }
         }
-        if (GetAsyncKeyState(VK_UP)){
-           if (!tastaUp){ tastaUp=true;
-              if (careFereastra==false && viewCounterStanga-1>=0){
-                 viewCounterStanga--;
-                 afisareFoldere(nrdiscStanga, discuri_localeStanga, viewCounterStanga, false);
-              }
-              else if(careFereastra==true && viewCounterDreapta-1>=0) {
-                  viewCounterDreapta--;
-                  afisareFoldere(nrdiscDreapta, discuri_localeDreapta, viewCounterDreapta, true);
-              }
-           }
-           tastaUp=false;
-           Sleep(300);
+        else
+        {
+            tastaEnter=false;
         }
+        ///modific selected
+        ///DownArrow
+        if (GetAsyncKeyState(VK_DOWN))
+        {
+            if (!tastaDown)
+            {
+                tastaDown=true;
+
+                if (careFereastra==false && viewCounterStanga+1<nr_foldereStanga+nr_fisiereStanga)
+                {
+                    viewCounterStanga++;
+                    afisare(foldereStanga,fisiereStanga,nr_foldereStanga,nr_fisiereStanga,viewCounterStanga,15);
+
+
+                }
+                else if(careFereastra==true && viewCounterDreapta+1<nr_foldereDreapta+nr_fisiereDreapta)
+                {
+                    viewCounterDreapta++;
+                    afisare(foldereDreapta,fisiereDreapta,nr_foldereDreapta,nr_fisiereDreapta,viewCounterDreapta,542);
+                }
+            }
+        }
+        else
+        {
+            tastaDown=false;
+        }
+
+        ///UpArrow
+        if (GetAsyncKeyState(VK_UP))
+        {
+            if (!tastaUp)
+            {
+                tastaUp=true;
+
+                if (careFereastra==false && viewCounterStanga-1>=0)
+                {
+                    viewCounterStanga--;
+                    afisare(foldereStanga,fisiereStanga,nr_foldereStanga,nr_fisiereStanga,viewCounterStanga,15);
+                }
+                else if(careFereastra==true && viewCounterDreapta-1>=0)
+                {
+                    viewCounterDreapta--;
+                    afisare(foldereDreapta,fisiereDreapta,nr_foldereDreapta,nr_fisiereDreapta,viewCounterDreapta,542);
+                }
+            }
+        }
+        else
+        {
+            tastaUp=false;
+        }
+
+
+
         if (GetAsyncKeyState(VK_LBUTTON))
         {
             if (!mousePress)
@@ -212,18 +404,32 @@ void utilizareaAplicatiei()
                     ///Am apasat cu mouse-ul butonul ESC
                     loop=false;
             }
+        }
+        else
+        {
             mousePress=false;
         }
+
+        ///ESC
         if (GetAsyncKeyState(VK_ESCAPE))
         {
             if (!tastaEsc)
+            {
                 tastaEsc=true;
-            loop=false;
+                loop=false;
+            }
         }
+        else
+        {
+            tastaEsc = false;
+        }
+
         if (GetAsyncKeyState(VK_TAB))
         {
-            if (!tastaTab) tastaTab=true;
+            if (!tastaTab)
             {
+                tastaTab=true;
+
                 if (careFereastra==false)
                 {
                     careFereastra=true;
@@ -241,10 +447,18 @@ void utilizareaAplicatiei()
                     rectangle(537, 80, 1013, 687);
                 }
             }
+        }
+        else
+        {
             tastaTab=false;
-            Sleep(300);
         }
     }
+
+    ///Eliberam memoria
+    free(foldereStanga);
+    free(fisiereStanga);
+    free(foldereDreapta);
+    free(fisiereDreapta);
 }
 
 int main()
